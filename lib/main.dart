e912a2acbd34e3d5f9863a8f22b792c8f9db259a
Sijan Panta <cjan1102@gmail.com>
2024-02-15 19:55:32 +0545

    add :loginpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,94 +1,137 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  )
+);
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  static const String _title = 'Flutter Stateful Clicker Counter';
-  // This widget is the root of your application.
+class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.amber,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-  // This class is the configuration for the state.
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Text('Flutter Demo Click Counter'),
-        backgroundColor: Colors.amber[200],
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: const TextStyle(fontSize: 25),
-            ),
-          ],
+      
+      body: Container(
+        
+        padding: EdgeInsets.symmetric(vertical: 30),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+           colors: [
+            const Color.fromARGB(255, 122, 72, 54),
+            Color.fromARGB(255, 112, 84, 75),
+            Color.fromARGB(255, 145, 101, 87),
+         
+        
+
+           ]
+            )
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-        backgroundColor: Colors.amber[200],
-      ),
-    );
+        child:Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 80,),
+            Padding(padding: EdgeInsets.all(20),
+            child: Column(children: <Widget>[
+              Text("Login", style:TextStyle(color:Colors.white ,fontSize:40),),
+              SizedBox(height: 10,),
+              Text("Welcome Back", style:TextStyle(color:Colors.white ,fontSize:21),)
+            ]
+            ),)
+,            SizedBox(height: 20,),
+                    Expanded(
+                      flex: 5,
+                      child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(40),
+                            topRight: Radius.circular(40),
+
+                          )),
+                          child:Padding(
+                            padding: const EdgeInsets.all(24),
+                          
+                           child:Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          TextField(
+                            style: TextStyle(color: Colors.black),
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: BorderSide.none,
+                              ),
+                              
+                              filled: true,
+                              fillColor: Colors.grey[250],
+                              hintText: "E-mail",
+                              hintStyle: TextStyle(fontWeight: FontWeight.bold),
+                              prefixIcon: Icon(
+                                Icons.email,
+                                color:Color.fromARGB(255, 134, 117, 111),
+                              )
+                            ),
+                          ),
+                          SizedBox(height: 20,),
+
+                          TextField(
+                            obscureText: true,
+                            keyboardType: TextInputType.visiblePassword,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: BorderSide.none,
+                              ),
+                              filled: true,
+                              fillColor: Colors.grey[250],
+                              hintText: "Password",
+                              hintStyle: TextStyle(fontWeight: FontWeight.bold),
+                              prefixIcon: Icon(
+                                Icons.lock,
+                                color:Color.fromARGB(255, 134, 117, 111),
+                              ),
+                            ),
+                          ),
+                           SizedBox(height: 40,),
+                           Text("Forgot Your password", style: TextStyle(color: Colors.blue),) ,
+                           SizedBox(height: 50,),
+                           Container(
+                            height: 50,
+                            margin: EdgeInsets.symmetric(horizontal: 40),
+                            decoration: BoxDecoration(
+                            borderRadius:BorderRadius.circular(50),
+                            color: Colors.blue,
+                            ),
+                            child: Center(
+                              child: Text("Login",style: TextStyle(color: const Color.fromARGB(255, 0, 0, 0)),) ,
+                              ),
+                            ),
+                           SizedBox(height: 80,),
+                           Row(
+                            children: <Widget>[
+                              Container(
+                                height: 50,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(50),
+                                  color: Colors.black
+                                ),
+                              )
+                            ],
+                           )
+                        ]
+                           ),
+                          ),
+                      ),
+                     
+                            
+              )],
+            ),
+          ));
   }
 }
