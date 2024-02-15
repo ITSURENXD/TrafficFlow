64a1b9672da886069ebba0662afebaac652620b6
Sandip Gyawali <sandipgyawali10@gmail.com>
2024-02-15 19:08:49 +0545

    added the icons, enhanced the box-shadow and changed the fontsize of the icons

--- lib/widgets/CustomBottomNavBar.dart
+++ lib/widgets/CustomBottomNavBar.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
-import 'package:line_icons/line_icons.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CustomBottomNavBar extends StatelessWidget {
-  const CustomBottomNavBar({Key? key}) : super(key: key);
+  final double iconSize; //icon size
+  final Color color; //icon color, text color,
+
+  const CustomBottomNavBar({
+    this.iconSize = 18.0,
+    this.color = const Color(0xffC67C4E),
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
         color: Colors.white,
         boxShadow: [
-          BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(0.1)),
+          BoxShadow(
+            blurRadius: 20,
+            color: Colors.black.withOpacity(0.23),
+          ),
         ],
       ),
       child: SafeArea(
@@ -22,15 +34,31 @@ class CustomBottomNavBar extends StatelessWidget {
             rippleColor: Colors.grey[300]!,
             hoverColor: Colors.grey[100]!,
             gap: 8,
-            iconSize: 24,
+            iconSize: iconSize,
             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
             tabBackgroundColor: Colors.grey[100]!,
-            color: Colors.black,
+            color: Colors.grey[600],
             tabs: [
-              GButton(icon: LineIcons.home, text: "Home"),
-              GButton(icon: LineIcons.heart, text: "Likes"),
-              GButton(icon: LineIcons.user, text: "Profile"),
-              GButton(icon: LineIcons.cog, text: "Settings"),
+              GButton(
+                icon: FontAwesomeIcons.house,
+                text: "Home",
+                iconActiveColor: color,
+              ),
+              GButton(
+                icon: FontAwesomeIcons.message,
+                text: "Queries",
+                iconActiveColor: color,
+              ),
+              GButton(
+                icon: FontAwesomeIcons.userAstronaut,
+                text: "Profile",
+                iconActiveColor: color,
+              ),
+              GButton(
+                icon: FontAwesomeIcons.gear,
+                text: "Settings",
+                iconActiveColor: color,
+              ),
             ],
           ),
         ),
