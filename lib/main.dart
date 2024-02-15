bac8b66315799bc2539ab7a29fa0c99cb8a45502
Sandip Gyawali <sandipgyawali10@gmail.com>
2024-02-15 19:05:51 +0545

    added the title inside the material app and updated the dependency: font_awesome_flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
-import "./screens/HomePage.dart";
+import 'package:flutter/services.dart';
+import 'screens/Home/HomePage.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+    ),
+  );
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,6 +20,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Traffic Flow",
+      theme: ThemeData.light().copyWith(
+        scaffoldBackgroundColor: Colors.white,
+      ),
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
