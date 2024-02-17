23577bf43b1f277b5a8acd357dcdfdf1447ed2de
Sijan Panta <cjan1102@gmail.com>
2024-02-17 20:38:34 +0545

    Login and signup pages UI is done ,you can initaite the backend

--- lib/screens/Login/LoginPage.dart
+++ lib/screens/Login/LoginPage.dart
@@ -1,137 +1,181 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-  MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: LoginPage(),
-  )
-);
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
 
-class LoginPage extends StatelessWidget{
+class _LoginPageState extends State<LoginPage> {
+  Color textColor = Colors.blue; // Initial color of the text
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       body: Container(
-        
         padding: EdgeInsets.symmetric(vertical: 30),
         width: double.infinity,
         decoration: BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
-           colors: [
-            const Color.fromARGB(255, 122, 72, 54),
-            Color.fromARGB(255, 112, 84, 75),
-            Color.fromARGB(255, 145, 101, 87),
-         
-        
-
-           ]
-            )
+            colors: [
+              Color(0xff131313),
+              Color.fromARGB(255, 12, 12, 12),
+            ],
+          ),
         ),
-        child:Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            SizedBox(height: 80,),
-            Padding(padding: EdgeInsets.all(20),
-            child: Column(children: <Widget>[
-              Text("Login", style:TextStyle(color:Colors.white ,fontSize:40),),
-              SizedBox(height: 10,),
-              Text("Welcome Back", style:TextStyle(color:Colors.white ,fontSize:21),)
-            ]
-            ),)
-,            SizedBox(height: 20,),
-                    Expanded(
-                      flex: 5,
-                      child: Container(
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(40),
-                            topRight: Radius.circular(40),
-
-                          )),
-                          child:Padding(
-                            padding: const EdgeInsets.all(24),
-                          
-                           child:Column(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 80,),
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Login",
+                      style: TextStyle(color: Colors.white, fontSize: 40),
+                    ),
+                    SizedBox(height: 5), // Add some vertical spacing between the texts
+                    Text(
+                      "Welcome Back",
+                      style: TextStyle(color: Colors.white, fontSize: 21),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20,),
+              Container(
+                height: MediaQuery.of(context).size.height - 230, // Adjust this height as needed
+                child: Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40),
+                        topRight: Radius.circular(40),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(24),
+                      child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          TextField(
-                            style: TextStyle(color: Colors.black),
+                          TextFormField(
+                            style: TextStyle(color: Colors.white),
                             keyboardType: TextInputType.emailAddress,
                             decoration: InputDecoration(
                               border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(8),
-                                borderSide: BorderSide.none,
+                                borderSide: BorderSide(color: Colors.white), // White border
                               ),
-                              
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: BorderSide(color: Colors.white), // White border
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: BorderSide(color: Colors.brown), // Brown border when focused
+                              ),
                               filled: true,
-                              fillColor: Colors.grey[250],
+                              fillColor: Colors.grey[600],
                               hintText: "E-mail",
-                              hintStyle: TextStyle(fontWeight: FontWeight.bold),
+                              hintStyle: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
                               prefixIcon: Icon(
                                 Icons.email,
-                                color:Color.fromARGB(255, 134, 117, 111),
-                              )
+                                color: Colors.white,
+                              ),
                             ),
                           ),
                           SizedBox(height: 20,),
-
-                          TextField(
+                          TextFormField(
                             obscureText: true,
                             keyboardType: TextInputType.visiblePassword,
                             decoration: InputDecoration(
                               border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(8),
-                                borderSide: BorderSide.none,
+                                borderSide: BorderSide(color: Colors.white), // White border
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: BorderSide(color: Colors.white), // White border
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: BorderSide(color: Colors.brown), // Brown border when focused
                               ),
                               filled: true,
-                              fillColor: Colors.grey[250],
+                              fillColor: Colors.grey[600],
                               hintText: "Password",
-                              hintStyle: TextStyle(fontWeight: FontWeight.bold),
+                              hintStyle: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
                               prefixIcon: Icon(
                                 Icons.lock,
-                                color:Color.fromARGB(255, 134, 117, 111),
+                                color: Colors.white,
                               ),
                             ),
                           ),
-                           SizedBox(height: 40,),
-                           Text("Forgot Your password", style: TextStyle(color: Colors.blue),) ,
-                           SizedBox(height: 50,),
-                           Container(
+                          SizedBox(height: 40,),
+                          GestureDetector(
+                            onTap: () {
+                              // Change text color to indicate tap
+                              setState(() {
+                                textColor = Colors.grey; // Change color to red
+                              });
+
+                              // Add your logic for "Forgot Your Password" action here
+                            
+                            },
+                            child: Text("Forgot Your password", style: TextStyle(color: Colors.black)), // Use textColor variable
+                          ),
+                          SizedBox(height: 20,),
+                          Container(
                             height: 50,
                             margin: EdgeInsets.symmetric(horizontal: 40),
                             decoration: BoxDecoration(
-                            borderRadius:BorderRadius.circular(50),
-                            color: Colors.blue,
+                              borderRadius: BorderRadius.circular(50),
+                              color: Color(0xff131313),
                             ),
                             child: Center(
-                              child: Text("Login",style: TextStyle(color: const Color.fromARGB(255, 0, 0, 0)),) ,
-                              ),
+                              child: Text("Login", style: TextStyle(color: Colors.white,fontSize: 23)),
                             ),
-                           SizedBox(height: 80,),
-                           Row(
+                          ),
+                          SizedBox(height: 20,),
+                          Container(
+                            height: 50,
+                            margin: EdgeInsets.symmetric(horizontal: 40),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: Color.fromARGB(255, 51, 51, 51), // Change color according to your preference
+                            ),
+                            child: Center(
+                              child: Text("Sign Up", style: TextStyle(color: Colors.white,fontSize: 20)),
+                            ),
+                          ),
+                          SizedBox(height: 20,),
+                          Row(
                             children: <Widget>[
                               Container(
                                 height: 50,
                                 decoration: BoxDecoration(
                                   borderRadius: BorderRadius.circular(50),
-                                  color: Colors.black
+                                  color: Colors.black,
                                 ),
                               )
                             ],
-                           )
-                        ]
-                           ),
-                          ),
+                          )
+                        ],
                       ),
-                     
-                            
-              )],
-            ),
-          ));
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/Login/signuppage.dart
+++ lib/screens/Login/signuppage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class SignUpPage extends StatelessWidget {
+class SignupPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,10 +11,11 @@ class SignUpPage extends StatelessWidget {
           gradient: LinearGradient(
             begin: Alignment.topCenter,
             colors: [
-              const Color.fromARGB(255, 122, 72, 54),
-              Color.fromARGB(255, 112, 84, 75),
-              Color.fromARGB(255, 145, 101, 87),
-            ],
+              Color(0xff131313),
+              Color.fromARGB(255, 12, 12, 12),
+              
+            ]
+            ,
           ),
         ),
         child: SingleChildScrollView(
@@ -25,10 +26,17 @@ class SignUpPage extends StatelessWidget {
               Padding(
                 padding: EdgeInsets.all(20),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text("Sign Up", style: TextStyle(color: Colors.white, fontSize: 40)),
-                    SizedBox(height: 10,),
-                    Text("Create an Account", style: TextStyle(color: Colors.white, fontSize: 21))
+                    Text(
+                      "Sign Up",
+                      style: TextStyle(color: Colors.white, fontSize: 40),
+                    ),
+                    SizedBox(height: 5), // Add some vertical spacing between the texts
+                    Text(
+                      "Create an Account",
+                      style: TextStyle(color: Colors.white, fontSize: 21),
+                    ),
                   ],
                 ),
               ),
@@ -44,6 +52,7 @@ class SignUpPage extends StatelessWidget {
                         topLeft: Radius.circular(40),
                         topRight: Radius.circular(40),
                       ),
+                      
                     ),
                     child: Padding(
                       padding: const EdgeInsets.all(24),
@@ -51,40 +60,56 @@ class SignUpPage extends StatelessWidget {
                         mainAxisAlignment: MainAxisAlignment.center,
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          TextField(
-                            style: TextStyle(color: Colors.black),
+                          TextFormField(
+                            style: TextStyle(color: Colors.white),
                             keyboardType: TextInputType.emailAddress,
                             decoration: InputDecoration(
                               border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(8),
-                                borderSide: BorderSide.none,
+                                borderSide: BorderSide(color: Colors.white), // White border
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: BorderSide(color: Colors.white), // White border
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: BorderSide(color: Colors.brown), // brown border when focused
                               ),
                               filled: true,
-                              fillColor: Colors.grey[250],
+                              fillColor: Colors.grey[600],
                               hintText: "E-mail",
-                              hintStyle: TextStyle(fontWeight: FontWeight.bold),
+                              hintStyle: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
                               prefixIcon: Icon(
                                 Icons.email,
-                                color: Color.fromARGB(255, 134, 117, 111),
+                                color: Colors.white,
                               ),
                             ),
                           ),
                           SizedBox(height: 20,),
-                          TextField(
+                          TextFormField(
                             obscureText: true,
                             keyboardType: TextInputType.visiblePassword,
                             decoration: InputDecoration(
                               border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(8),
-                                borderSide: BorderSide.none,
+                                borderSide: BorderSide(color: Colors.white), // White border
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: BorderSide(color: Colors.white), // White border
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                borderSide: BorderSide(color: Colors.brown), // brown border when focused
                               ),
                               filled: true,
-                              fillColor: Colors.grey[250],
+                              fillColor: Colors.grey[600],
                               hintText: "Password",
-                              hintStyle: TextStyle(fontWeight: FontWeight.bold),
+                              hintStyle: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
                               prefixIcon: Icon(
                                 Icons.lock,
-                                color: Color.fromARGB(255, 134, 117, 111),
+                                color: Colors.white,
                               ),
                             ),
                           ),
@@ -94,24 +119,32 @@ class SignUpPage extends StatelessWidget {
                             margin: EdgeInsets.symmetric(horizontal: 40),
                             decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(50),
-                              color: Colors.blue,
+                              color: Color(0xff131313),
                             ),
                             child: Center(
-                              child: Text("Sign Up", style: TextStyle(color: Colors.black)),
+                              child: Text("Sign Up", style: TextStyle(color: Colors.white,fontSize: 23)),
                             ),
                           ),
                           SizedBox(height: 20,),
                           Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: <Widget>[
-                              Container(
-                                height: 50,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(50),
-                                  color: Colors.black,
+                              Text(
+                                "Already have an account? ",
+                                style: TextStyle(color: Colors.grey[600]),
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  // Navigate to the login page
+                                  Navigator.pop(context);
+                                },
+                                child: Text(
+                                  "Login here",
+                                  style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
                                 ),
-                              )
+                              ),
                             ],
-                          )
+                          ),
                         ],
                       ),
                     ),
